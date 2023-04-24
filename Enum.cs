a9dbafb5e691285de3e00777ea83207e81992515
Artorias718 <lai.ndr94@gmail.com>
2023-04-24 19:22:14 +0200

    aggiunto dizionario per la presenza delle parole nelle risorse lessicali per sentimento

---
+++ Enum.cs
@@ -0,0 +1,20 @@
+using System;
+
+public enum Emotions
+{
+    anger,
+    anticipation,
+    disgust,
+    fear,
+    joy,
+    sadness,
+    trust
+}
+
+public enum Resources
+{
+    EmoSN,
+    NRC,
+    sentisense,
+    nuova_risorsa
+}
--- Program.cs
+++ Program.cs
@@ -7,99 +7,162 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            HashSet<string> posemoticons = new HashSet<string>(File.ReadAllLines("Risorse lessicali/posemoticons.txt"));
-            HashSet<string> negemoticons = new HashSet<string>(File.ReadAllLines("Risorse lessicali/negemoticons.txt"));
-
             string[] splittedTextPos = Utils.ExtractEmoji("Risorse lessicali/posemoticons.txt");
             string[] splittedTextNeg = Utils.ExtractEmoji("Risorse lessicali/negemoticons.txt");
             string[] splittedText = splittedTextPos.Concat(splittedTextNeg).ToArray();
 
-            Parsing.readTwitter("fare.txt", splittedText);
+            //Parsing.readTwitter("fare.txt", splittedText);
 
 
+            Dictionary<string, string> lemmaDictionary = new Dictionary<string, string>();
+            //per ogni emozione accede a ogni risorsa (EMOsn, NRC ecc.)
+            foreach (Emotions em in Enum.GetValues(typeof(Emotions)))
+            {
+                //Console.WriteLine(em);
+                Dictionary<string, Dictionary<string, int>> lemmi = new Dictionary<string, Dictionary<string, int>>();
+                string startPath = $"Risorse lessicali/{em}/";
+                string endPath = $"_{em}.txt";
+
+                foreach (Resources res in Resources.GetValues(typeof(Resources)))
+                {
+                    if (res == Resources.nuova_risorsa)
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        string resString = res.ToString();
+                        string path = startPath + resString + endPath;
+
+                        // Leggi tutti i lemmi presenti nella risorsa lessicale
+                        using (StreamReader reader = new StreamReader(path))
+                        {
+                            string lemma = reader.ReadLine();
+                            while (lemma != null)
+                            {
+                                // Rimuovi l'endline
+                                lemma = lemma.Replace("\n", "");
+
+                                // Rimuovi le parole composte
+                                if (!lemma.Contains("_"))
+                                {
+                                    Dictionary<string, int> l = lemmi.ContainsKey(lemma) ? lemmi[lemma] : new Dictionary<string, int>();
+                                    l[resString] = 1;
+                                    lemmi[lemma] = l;
+                                }
+
+                                lemma = reader.ReadLine();
+
+                            }
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        // Continua se il file non è stato trovato
+                        continue;
+                    }
+
+                }
+                //var lemmiList = lemmi.Select(kv => new { lemma = kv.Key, risorse = kv.Value }).ToList();
+
+                foreach (var kv in lemmi)
+                {
+                    Console.WriteLine("Lemma: " + kv.Key);
+                    Console.WriteLine("Risorse:");
+                    foreach (var risorsa in kv.Value)
+                    {
+
+                        Console.WriteLine(risorsa.Key + ": " + risorsa.Value);
+
+                    }
+                    Console.WriteLine();
+                }
+
+
+            }
 
         }
 
-    }
-
-    public static class Parsing
-    {
-        public static void readTwitter(string nameFile, string[] splittedText)
+        public static class Parsing
         {
-
-            String line;
-            try
+            public static void readTwitter(string nameFile, string[] splittedText)
             {
-                StreamReader sr = new StreamReader(nameFile);
 
-                line = sr.ReadToEnd();
-                int i = line.IndexOf("_");
-                int head;
-                int tail;
-
-                while (i > 0)
+                String line;
+                try
                 {
-                    Console.WriteLine("Underscore: " + i);
-                    int j = i;
+                    StreamReader sr = new StreamReader(nameFile);
 
-                    while (!line[j].Equals(' '))
+                    line = sr.ReadToEnd();
+                    int i = line.IndexOf("_");
+                    int head;
+                    int tail;
+
+                    while (i > 0)
                     {
-                        j--;
-                    }
-                    head = j;
+                        Console.WriteLine("Underscore: " + i);
+                        int j = i;
 
-                    Console.WriteLine("Head: " + head);
-                    j = i;
-                    while (!line[j].Equals(' ') && j < line.Length - 1)
-                    {
-                        j++;
-                    }
-                    tail = j;
-                    Console.WriteLine("Tail: " + j);
-
-                    //salva la parola che stiamo considerando, per controllare se è un emoji
-                    String selected_word = line.Substring(head + 1, tail - head - 1);
-
-
-                    bool isEmoji = false;
-
-                    //conftronta ogni emoji con la parola, sia per le emoji positive che per quelle negative
-                    foreach (String emoji in splittedText)
-                    {
-                        // Console.WriteLine("Confronta " + selected_word + " con " + emoji + " Risultato: " + selected_word.Equals(emoji));
-                        if (selected_word.Equals(emoji))
+                        while (!line[j].Equals(' '))
                         {
-                            isEmoji = true;
+                            j--;
+                        }
+                        head = j;
+
+                        Console.WriteLine("Head: " + head);
+                        j = i;
+                        while (!line[j].Equals(' ') && j < line.Length - 1)
+                        {
+                            j++;
+                        }
+                        tail = j;
+                        Console.WriteLine("Tail: " + j);
+
+                        //salva la parola che stiamo considerando, per controllare se è un emoji
+                        String selected_word = line.Substring(head + 1, tail - head - 1);
+
+
+                        bool isEmoji = false;
+
+                        //conftronta ogni emoji con la parola, sia per le emoji positive che per quelle negative
+                        foreach (String emoji in splittedText)
+                        {
+                            // Console.WriteLine("Confronta " + selected_word + " con " + emoji + " Risultato: " + selected_word.Equals(emoji));
+                            if (selected_word.Equals(emoji))
+                            {
+                                isEmoji = true;
+
+                            }
 
                         }
 
+                        if (!isEmoji)
+                        {
+                            String res = line.Remove(head + 1, tail - head);
+                            sr.Close();
+
+                            File.WriteAllText("fare.txt", File.ReadAllText("fare.txt").Replace(line, res));
+                            line = res; //salva la nuova frase per continuare a iterare
+                        }
+
+
+                        i = line.IndexOf("_", i + 1);
                     }
 
-                    if (!isEmoji)
-                    {
-                        String res = line.Remove(head + 1, tail - head);
-                        sr.Close();
+                    sr.Close();
 
-                        File.WriteAllText("fare.txt", File.ReadAllText("fare.txt").Replace(line, res));
-                        line = res; //salva la nuova frase per continuare a iterare
-                    }
-
-
-                    i = line.IndexOf("_", i + 1);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
                 }
 
-                sr.Close();
-
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
-            }
-
         }
     }
 }
